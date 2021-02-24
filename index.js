2c5e4afb417788b6d8ae814d99aaed47af194758
Ben Hovinga <agnivohneb@gmail.com>
2021-02-24 14:04:26 -0400

    Dicee Challenge Step 4 Change the <img> to a Random Dice

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
 function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
+
+/**
+ * Replaces the dice image in the DOM
+ * @param {string} imageSelector - The DOM selector of the image to change ex. ".img1"
+ * @param {number} diceRoll - The dice number to change to
+ * @returns {boolean} Success state
+ **/
+function replaceDiceImage(imageSelector, diceRoll) {
+  if (
+    typeof diceRoll !== "number" ||
+    diceRoll > 6 ||
+    diceRoll < 1 ||
+    imageSelector === null ||
+    imageSelector === undefined
+  ) {
+    return false;
+  }
+
+  document.querySelector(imageSelector).src = "images/dice" + diceRoll + ".png";
+  return true;
+}
